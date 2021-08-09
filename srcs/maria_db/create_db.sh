af9ac48f0d4d142a643c4fe5419aeb902c2d0c2b
Berthet thomas <thoberth@student.42.fr>
2021-08-09 20:44:31 +0200

    ajout de wp-config.php et autre modif

--- srcs/maria_db/create_db.sh
+++ srcs/maria_db/create_db.sh
@@ -1,17 +1,23 @@
-#export MYSQL_DATABASE=db
-#export MYSQL_USER=root
-#export DOMAIN_NAME=thoberth.42.fr
-#export MYSQL_PASSWORD=@123toto
+#!/bin/bash
+
+echo "initialization of database"
+sleep 2
 
 mysqld_safe &
-echo "db initialised"
+
 echo "wait 15s"
 sleep 15
+
 echo "ajout de la db"
+sleep 2
 mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mysql -e "GRANT ALL ON db.* TO ${MYSQL_USER}@${DOMAIN_NAME} IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
+
 echo "database shutdown"
+sleep 2
 mysqladmin shutdown
+
 echo "database restarting"
+sleep 2
 exec mysqld -u root 
